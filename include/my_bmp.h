125bf5caf06b1f10dec64f4a3b6bcafdd2f7cb71
Julien Thevenon <julien_thevenon@yahoo.fr>
2013-11-13 17:50:16 +0100

    Add header protection

--- include/my_bmp.h
+++ include/my_bmp.h
@@ -1,3 +1,21 @@
+/*    This file is part of lib_bmp
+      The aim of this software is to provide way to interact with BMP
+      files
+      Copyright (C) 2013  Julien Thevenon ( julien_thevenon at yahoo.fr )
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +24,8 @@
 #include <math.h>
 #include <assert.h>
 
+#ifndef MY_BMP_H
+#define MY_BMP_H
 class my_color
 {
  public:
@@ -860,4 +880,6 @@ class my_bmp
       my_palette *m_palette;
       my_color_alpha * m_content;
 };
+
+#endif // MY_BMP_H
 //EOF
